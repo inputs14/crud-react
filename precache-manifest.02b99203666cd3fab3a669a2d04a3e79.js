e5aa0d26b2715c1bd353891a84fa47d1e8b8fd54
SK851831 <sk851831@gmail.com>
2019-12-27 05:14:25 +0530

    Updates

--- precache-manifest.02b99203666cd3fab3a669a2d04a3e79.js
+++ precache-manifest.02b99203666cd3fab3a669a2d04a3e79.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "4503b56e9423e8fa088be8c4e1c0c182",
+    "revision": "45971a091444ff891a4fe80be0896f5d",
     "url": "/crud-react/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/crud-react/static/css/2.17e5ed98.chunk.css"
   },
   {
-    "revision": "a9557a5ab592085dc5e8",
+    "revision": "96e1cedbb37d9e5ec101",
     "url": "/crud-react/static/css/main.ce1d563c.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/crud-react/static/js/2.8ffff116.chunk.js"
   },
   {
-    "revision": "a9557a5ab592085dc5e8",
-    "url": "/crud-react/static/js/main.afacd6cb.chunk.js"
+    "revision": "96e1cedbb37d9e5ec101",
+    "url": "/crud-react/static/js/main.d1116f38.chunk.js"
   },
   {
     "revision": "3a0dd3648776c1707ef0",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/crud-react/precache-manifest.095c7287323d26a768ffccc2214fc67a.js"
+  "/crud-react/precache-manifest.02b99203666cd3fab3a669a2d04a3e79.js"
 );
 
 self.addEventListener('message', (event) => {
